1e8fdf8743df29a823e206dfafe016db4186360b
Gaillard Tsarasitraka <professionnalm@gmail.com>
2023-01-25 14:08:53 +0300

    lancement du app avec nodemon

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,10 @@
 const express = require('express'); 
+const app = express();
 const articlesController =  require('../db/controllers/article.controller');
-
 const router = express.Router();
 
-router.get("/",articlesController.index);
+router.get('/',articlesController.index);
+
+app.use("/", router);
 
 module.exports = router;
